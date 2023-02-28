0e87353dcdcab5cbb038c8f986e34f1d6969fcff
pedrocerrano <82617951+pedrocerrano@users.noreply.github.com>
2023-02-28 10:26:19 -0600

    Adds initial project to GitHub

---
+++ Pokedex_Codable/Pokedex_Codable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex_Codable
+//
+//  Created by iMac Pro on 2/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
